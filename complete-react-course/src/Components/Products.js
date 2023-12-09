36ac03de3a5eeb28497f8a20f026dc72b68a3939
juanpoquiomaesquivel <juanpoquiomaesquivel@hotmail.com>
2023-12-09 05:24:47 -0500

    react~(4) embedding expresions a& jsx attr
    - videos from 12 to 13

--- complete-react-course/src/Components/Products.js
+++ complete-react-course/src/Components/Products.js
@@ -1,25 +1,45 @@
+let productCount = 0;
+let imageUrl = require("../Images/fresh-milk.png");
+/* let style = {
+    padding: "0 20px",
+    // fontSize: 12
+    'font-size': 12
+}; */
+
+function displayFormattedProductCount() {
+    return productCount > 0 ? productCount : "Zero";
+}
+
 function Products() {
     return (
         <ul className="list-group shadow">
             <li className="list-group-item">
-                <div className="media align-items-lg-center flex-column flex-lg-row p-3">
+                <div className="media align-items-lg-center flex-column flex-lg-row p-3 d-flex">
                     <div className="media-body order-2 order-lg-1">
-                        <h5 className="mt-0 fw-bold mb-2">
-                            Fresh Milk
-                        </h5>
+                        <h5 className="mt-0 fw-bold mb-2">Fresh Milk</h5>
                         <p className="font-italic text-muted mb-0 small">
                             Lorem, ipsum dolor sit amet consectetur adipisicing
                             elit. Tempora minima vero asperiores nulla. Quis
                             nobis omnis error qui eum temporibus cum a enim
                             ullam beatae, facilis, unde neque quia et?
                         </p>
-                        <div className="d-flex align-items-center justify-content-between mt-1">
-                            <h6 className="fw-bold my-2">$120.00</h6>
+                        <div className="d-flex align-items-center justify-content-start mt-1">
+                            <h6
+                                className="fw-bold my-2"
+                                style={{ marginRight: 30 }}
+                            >
+                                $120.00
+                            </h6>
+                            <button className="btn btn-primary">-</button>
+                            <span style={{ padding: "8px 14px", fontSize: 13 }}>
+                                {displayFormattedProductCount()}
+                            </span>
+                            <button className="btn btn-primary">+</button>
                         </div>
                     </div>
                     <img
-                        src=""
-                        alt=""
+                        src={imageUrl}
+                        alt="Generic"
                         width="100"
                         className="ml-lg-5 order-1 order-lg-2"
                     />
--- complete-react-course/src/index.js
+++ complete-react-course/src/index.js
@@ -16,7 +16,6 @@ root.render(<App></App>);
 // let div = document.getElementById('root');
 // div.append(App());
 
-/* Child Components & complex jsx & bootstrap #3
-https://www.youtube.com/watch?v=FtpXyC3LXds&list=PL1BztTYDF-QMPh8UW3xxk8C6uk2WVfUS3&index=9&pp=iAQB
-https://www.youtube.com/watch?v=JI-dxkq7ezI&list=PL1BztTYDF-QMPh8UW3xxk8C6uk2WVfUS3&index=10&pp=iAQB
-https://www.youtube.com/watch?v=qy9MxbQ2GUQ&list=PL1BztTYDF-QMPh8UW3xxk8C6uk2WVfUS3&index=11&pp=iAQB */
+/* Embedding expresions & JSX Attributes #4
+https://www.youtube.com/watch?v=tQyMmB50coo&list=PL1BztTYDF-QMPh8UW3xxk8C6uk2WVfUS3&index=12&pp=iAQB
+https://www.youtube.com/watch?v=BvBf8GjsYdw&list=PL1BztTYDF-QMPh8UW3xxk8C6uk2WVfUS3&index=13&pp=iAQB */
